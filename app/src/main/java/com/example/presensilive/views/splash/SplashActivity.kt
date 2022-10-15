705725940aa078692d6e2ee5be1161b964823534
keuanganbonebolango <keuangan.bonebolango@gmail.com>
2022-10-16 07:00:35 +0800

    ui : add splash screen

---
+++ app/src/main/java/com/example/presensilive/views/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.presensilive.views.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.presensilive.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
